08a520e7791da47db6b801d29044bff5d9e72a68
Guilherme <teluski@MacBook-Pro-de-Guilherme.local>
2026-06-05 01:08:28 -0300

    fix: bind server.js to 0.0.0.0 for Hostinger

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+const hostname = process.env.HOSTNAME || '0.0.0.0';
 // A Hostinger passa a porta através da variável de ambiente PORT
 const port = process.env.PORT || 3000;
 
